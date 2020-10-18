570b29856c474b73610b21964a4468d95d70be08
INOCcent-afk <michael828inoc@gmail.com>
2020-10-19 00:24:14 +0800

    Updates

---
+++ precache-manifest.05b8968a678e32fe059a3fe5f287466c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155c35d4d7707e45bb5cf1461c4e38c6",
+    "url": "/second-react-website/index.html"
+  },
+  {
+    "revision": "bbaf244a9074eeea91ab",
+    "url": "/second-react-website/static/css/main.7225a45f.chunk.css"
+  },
+  {
+    "revision": "e299ac302dacfcda2ee4",
+    "url": "/second-react-website/static/js/2.2dbac5c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/second-react-website/static/js/2.2dbac5c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbaf244a9074eeea91ab",
+    "url": "/second-react-website/static/js/main.748e97c2.chunk.js"
+  },
+  {
+    "revision": "71fc3819ed3eee2f8a49",
+    "url": "/second-react-website/static/js/runtime-main.feb8533b.js"
+  }
+]);
